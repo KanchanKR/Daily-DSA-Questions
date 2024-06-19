7eefa528b56e61088713f5e7c5bb744eae7717ad
KanchanKR <renapurkarkanchan@gmail.com>
2024-06-19 13:54:00 +0530

    Find maximum volume of a cuboid

---
+++ cuboid.java
@@ -0,0 +1,11 @@
+class Solution {
+
+    double maxVolume(double P, double A) {
+       double l = (double)(P - Math.sqrt(P * P - 24 * A)) / 12;
+ 
+    double height = (P / 4) - (2 * (l));
+        
+         double ans = l * l* height;
+          return ans;
+    }
+}

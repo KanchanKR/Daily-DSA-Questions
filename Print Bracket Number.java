0dd16071b768ee72d873e112d8228cef1f072a1f
KanchanKR <renapurkarkanchan@gmail.com>
2024-06-24 11:15:35 +0530

    add problem of that day

---
+++ Print Bracket Number.java
@@ -0,0 +1,27 @@
+
+
+// User function Template for Java
+
+import java.util.ArrayList;
+import java.util.Stack;
+
+class Solution {
+    ArrayList<Integer> bracketNumbers(String str) {
+        // code here
+        ArrayList<Integer> ans = new ArrayList<>();
+        Stack<Integer> st = new Stack<>();
+        int count=0;
+        for(int i=0;i<str.length();i++){
+            if(str.charAt(i)=='('){
+                count++;
+                ans.add(count);
+                st.push(count);
+            }
+            else if(str.charAt(i)==')'){
+                ans.add(st.peek());
+                st.pop();
+            }
+        }
+        return ans;
+    }
+};
